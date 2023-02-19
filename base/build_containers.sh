d4e7bcba8825972d71d346f27d32e49eb92e7d9d
Wilder Lopes <wilder@ogarantia.com>
2023-02-18 21:19:47 -0600

    Renaming containers.

--- base/build_containers.sh
+++ base/build_containers.sh
@@ -2,15 +2,15 @@
 
 OS="linux"
 
-for PLATFORM in "amd64" #"amd64"
+for PLATFORM in "arm64" "amd64"
 do
     # Build baseimage mini
     # docker buildx build --platform $PLATFORM -t ogarantia/ogre:baseimage-mini -f ./Dockerfile.baseimage_ogre_mini .
 
     # Build baseimage standard ubuntu22.04 - CPU
-    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-ubuntu22.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu22.04 .
+    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-$PLATFORM -f ./Dockerfile.base_ubuntu22.04 .
     # Build baseimage standard ubuntu22.04 - GPU
-    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-gpu-ubuntu22.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu22.04_gpu .
+    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-gpu-ubuntu22.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu22.04_gpu .
     #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-ubuntu20.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu20.04 .
     # Build baseimage standard ubuntu16.04
     #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-ubuntu16.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu16.04 .
