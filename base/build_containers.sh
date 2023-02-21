af132476cae22a2927469b4c38f263d27103bf27
Wilder Lopes <wilder@ogarantia.com>
2023-02-21 20:39:22 +0000

    Update dockerfiles.

--- base/build_containers.sh
+++ base/build_containers.sh
@@ -2,18 +2,19 @@
 
 OS="linux"
 
-for PLATFORM in "arm64" "amd64"
+for PLATFORM in "amd64" "arm64"
 do
-    # Build baseimage mini
-    # docker buildx build --platform $PLATFORM -t ogarantia/ogre:baseimage-mini -f ./Dockerfile.baseimage_ogre_mini .
-
     # Build baseimage standard ubuntu22.04 - CPU
     docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-$PLATFORM -f ./Dockerfile.base_ubuntu22.04 .
     # Build baseimage standard ubuntu22.04 - GPU
-    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-gpu-ubuntu22.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu22.04_gpu .
-    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-ubuntu20.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu20.04 .
-    # Build baseimage standard ubuntu16.04
-    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:baseimage-standard-ubuntu16.04 -f ./Dockerfile.baseimage_ogre_standard_ubuntu16.04 .
-    # Build baseimage standard gpu
-#    docker buildx build --platform $PLATFORM -t ogarantia/ogre:baseimage-standard-gpu -f ./Dockerfile.baseimage_ogre_standard_gpu .
+    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu22.04_gpu .
+    # Build baseimage standard ubuntu20.04 - CPU
+    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-$PLATFORM -f ./Dockerfile.base_ubuntu20.04 .
+    # Build baseimage standard ubuntu20.04 - GPU
+    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu20.04_gpu .
+    # Build baseimage standard ubuntu18.04 - CPU
+    # docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu18.04-$PLATFORM -f ./Dockerfile.base_ubuntu18.04 .
+    # Build baseimage standard ubuntu18.04 - GPU
+    # docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu18.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu18.04_gpu .
+
 done
