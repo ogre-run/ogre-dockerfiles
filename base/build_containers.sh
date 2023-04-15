20a3b22a6b7427ffe9a823c78e45ea67dbd080e3
Wilder Lopes <wilder@ogarantia.com>
2023-04-14 21:00:49 -0500

    Set jupyterlab theme as dark by default.
    Added base/overrides.json to set dark theme for jupyterlab.

--- base/build_containers.sh
+++ base/build_containers.sh
@@ -2,16 +2,16 @@
 
 OS="linux"
 
-for PLATFORM in "amd64" "arm64"
+for PLATFORM in "arm64"
 do
     # Build baseimage standard ubuntu22.04 - CPU
     docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-$PLATFORM -f ./Dockerfile.base_ubuntu22.04 .
     # Build baseimage standard ubuntu22.04 - GPU
-    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu22.04_gpu .
+    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu22.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu22.04_gpu .
     # Build baseimage standard ubuntu20.04 - CPU
-    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-$PLATFORM -f ./Dockerfile.base_ubuntu20.04 .
+    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-$PLATFORM -f ./Dockerfile.base_ubuntu20.04 .
     # Build baseimage standard ubuntu20.04 - GPU
-    docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu20.04_gpu .
+    #docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu20.04-gpu-$PLATFORM -f ./Dockerfile.base_ubuntu20.04_gpu .
     # Build baseimage standard ubuntu18.04 - CPU
     # docker buildx build --platform $OS/$PLATFORM -t ogarantia/ogre:base-ubuntu18.04-$PLATFORM -f ./Dockerfile.base_ubuntu18.04 .
     # Build baseimage standard ubuntu18.04 - GPU
